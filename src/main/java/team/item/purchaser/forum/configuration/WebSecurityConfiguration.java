88510e48f0440d1a30ee62b4659fa62d7cb05fe6
eloyhere <eloy.kim@outlook.com>
2023-01-28 21:16:18 +0800

    登录和注册

--- src/main/java/team/item/purchaser/forum/configuration/WebSecurityConfiguration.java
+++ src/main/java/team/item/purchaser/forum/configuration/WebSecurityConfiguration.java
@@ -16,7 +16,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import team.item.purchaser.forum.provider.AnonymousAuthenticationProvider;
 import team.item.purchaser.forum.provider.UsernamePasswordAuthenticationProvider;
 import team.item.purchaser.forum.service.*;
 
